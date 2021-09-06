5d11961a566033bdf4777e5e51093af9035b7e66
Alezrik <ex_ra@hotmail.com>
2021-09-06 03:37:06 -0700

    start project

---
+++ ue4/FantasyGame/Source/FantasyGameEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class FantasyGameEditorTarget : TargetRules
+{
+	public FantasyGameEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("FantasyGame");
+	}
+}
