0eeab3849226fb383f516a03591f45ded4b76285
Alezrik <ex_ra@hotmail.com>
2021-09-06 21:21:09 -0700

    add rest of files

---
+++ ue4/FantasyGame/Source/FantasyGame/FantasyGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "FantasyGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FANTASYGAME_API UFantasyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
