85df97f64e45cdb876d0c2739e39d486fb1febb2
Alezrik <ex_ra@hotmail.com>
2021-09-06 21:02:48 -0700

    more fix

---
+++ ue4/FantasyGame/Source/FantasyGame/FantasyGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FantasyGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FANTASYGAME_API AFantasyGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
